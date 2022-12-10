a6f1b32ad20dd53947597816b8a4fc889c0abcc2
Marcos Arturo Alape Diaz <ProgMarcosDiaz2022@gmail.com>
2022-12-09 19:45:20 -0500

    refixed error with particles js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
+// import ScriptTag from 'react-script-tag'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Aboutme from './pages/Aboutme'
 import Skills from './pages/Skills'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
+import { Helmet } from 'react-helmet'
 
 function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        <script src="./src//libraries/particles.min.js"></script>
+        <script src="./src/libraries/particlesConfig.js"></script>
+      </Helmet>
       <div id="particles-js"></div>
       <Navbar />
       <Home />
